aa9ee8cab07ddcf8c13d6d39b45cb9bf4150a620
Sivasai Silla <sivasai3920@gmail.com>
2024-07-13 14:11:27 +0530

    Day-1 Completed

---
+++ Day-1/index.js
@@ -0,0 +1,67 @@
+///////////////////////////
+//Activity-1
+///////////////////////////
+
+//Task-1
+var num = 7;
+console.log(num)
+
+//Task-2
+let str = "Sivasai";
+console.log(str);
+
+///////////////////////////
+//Activity-2
+///////////////////////////
+
+//Task-3
+const isCorrect = true;
+console.log(isCorrect);
+
+///////////////////////////
+//Activity-3
+///////////////////////////
+
+//Task-4
+ 
+const myNum = 7;
+console.log("myNum is of type " + typeof(myNum));
+
+const myName = "Sai";
+console.log("myName is of type " + typeof(myName));
+
+const myChoice = false;
+console.log("myChoice is of type " + typeof(myChoice));
+
+const myObj = {id:2};
+console.log("myObj is of type " + typeof(myObj));
+
+const myArr = [1,2,3,4,5];
+console.log("myArr is of type " + typeof(myArr));
+
+///////////////////////////
+//Activity-4
+///////////////////////////
+
+//Task-5
+
+let fruit = "Mango"
+console.log(fruit)
+fruit = "Apple";
+console.log(fruit);
+
+///////////////////////////
+//Activity-5
+///////////////////////////
+
+//Task-6
+
+try{
+    const pen = "Flair";
+    pen = "Hauser";
+}
+catch(err){
+    console.error(err.message)
+}
+
+
